795c1582d6605870e6e98d54064579d33bf461c5
busekeklik <busekeklik23@gmail.com>
2023-06-03 16:04:51 +0300

    added cpp08

---
+++ cpp08/ex00/easyfind.hpp
@@ -0,0 +1,15 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <vector>
+#include <iostream>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::exception();
+	return it;
+}
+#endif
